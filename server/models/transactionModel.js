c156e96605081de45859eeda59d48f458df35e29
Vaibhav Thakkar <vaibhav.thakkar.22.12.99@gmail.com>
2018-12-19 14:33:35 +0530

    Add transaction API

--- server/models/transactionModel.js
+++ server/models/transactionModel.js
@@ -6,9 +6,7 @@ const transactionSchema = new Schema({
     paidBy        : {type: String,   required: true},   // Id of user who paid
     description   : {type: String,   default: ""},      // Description String of transaction
     splitRatio   : {type: [Number], default: ""},      // Array of splitRatio's for all members in paidFor array
-    creator       : {type: String,   required: true},   // Person who initiated this transaction
     paidFor       : {type: [String], required: true},   // Array of Id's of users
-    includeSelf   : {type: Boolean,  required: true}    // Check if he also paid for himself to differ split from loan
 }, {timestamps: true})
 
 const Transaction = mongoose.model('transaction', transactionSchema)
--- server/routes/transactionRoutes.js
+++ server/routes/transactionRoutes.js
@@ -8,7 +8,7 @@ router.use(bodyParser.urlencoded({ extended: true }))
 
 // Handler to validate transaction api request
 const transactionReqHandler = function(req, res, next){
-    if(req.body.amount == undefined || (!req.body.paidBy) || (!req.body.paidFor)){
+    if((req.body.amount == undefined) || (!req.body.paidBy) || (!req.body.paidFor) || (!req.body.splitRatio) ){
         res.status(400)
         res.setHeader("Access-Control-Allow-Origin", "http://localhost")
         res.send("User Id not sent!")
@@ -17,56 +17,70 @@ const transactionReqHandler = function(req, res, next){
     }
 }
 
-// Endpoint for making a new transaction
-router.post('/new', transactionReqHandler, function(req, res){
-    var includeSelf = false;
-    if(req.body.paidFor.includes(req.body.paidBy)){
-        includeSelf = true
+function updateTransactions(paidBy, index, splitRatio, currUsers, sum, transactionId, amount){
+    if(index == splitRatio.length)
+        return
+    
+    currUsers[index]['transactions'].push(transactionId)
+    for(var i=0;i<currUsers[index].length;i++){
+        if(currUsers[index]['friends'][i]['Id'] == paidBy['Id']){
+            currUsers[index]['friends'][i]['currBalance'] = currUsers[index]['friends'][i]['currBalance'] - ((splitRatio[index]*amount)/sum)
+            for(var j=0;j<paidBy['friends'].length;j++){
+                if(paidBy['friends'][j]['Id'] == currUsers[index]['Id']){
+                    paidBy['friends'][j]['currBalance'] = paidBy['friends'][j]['currBalance'] + ((splitRatio[index]*amount)/sum)   
+                }
+            }
+        }
     }
 
-    //Check if all users exist-
-    req.body.paidFor.forEach(friend =>{
-        User.findOne({Id: friend}).then(function(currUser){
-            if(!currUser){
-                res.status(404)
-                res.setHeader("Access-Control-Allow-Origin", "http://localhost")
-                res.send("Friend does not exist");
-            }
-        })
-        .catch(function(err){
-            console.log(err)
-            res.status(500)
-            res.setHeader("Access-Control-Allow-Origin", "http://localhost")
-            res.send("Internal Server Error")
-        })
-    });
+    updateTransactions(paidBy, index+1, splitRatio, currUsers, sum, transactionId, amount)
+}
+
+// Endpoint for making a new transaction
+router.post('/new', transactionReqHandler, function(req, res){
+    var splitRatio = req.body.splitRatio.map(Number)
+    var sum = splitRatio.reduce(function(a, b) { return a + b; }, 0)
+    var amount = req.body.amount
+    for(var i=0; i<req.body.paidFor.length; i++){
+        if(req.body.paidFor[i] == req.body.paidBy){
+            amount = amount - ((splitRatio[i]*amount)/sum)
+            sum = sum - splitRatio[i];
+            splitRatio.splice(i, 1)
+            req.body.paidFor.splice(i, 1)
+        }
+    }
+
     User.findOne({Id: req.body.paidBy}).then(function(paidBy){
         if(!paidBy){
             res.status(404)
             res.setHeader("Access-Control-Allow-Origin", "http://localhost")
             res.send("User does not exist")
         } else {
-            new Transaction({
-                amount: req.body.amount,
-                paidBy: req.body.paidBy,
-                description: req.body.description,
-                paidFor: req.body.paidFor,
-                includeSelf: includeSelf
-            }).save().then(function(result){
-                console.log(result)
-                var perPerson = req.body.amount/req.body.paidFor.length;
+            //Check if all users exist-
+            User.find({Id: {$in: req.body.paidFor}}).then(function(currUsers){
+                if(!currUsers || currUsers.length != req.body.paidFor.length){
+                    res.status(404)
+                    res.setHeader("Access-Control-Allow-Origin", "http://localhost")
+                    res.send("Some user does not exist")
+                    return false
+                } else{
+                    new Transaction({
+                        amount: amount,
+                        paidBy: req.body.paidBy,
+                        description: req.body.description,
+                        splitRatio: splitRatio,
+                        paidFor: req.body.paidFor,
+                    }).save().then(function(result){
+                        // console.log(currUsers)
+
+                        updateTransactions(paidBy, 0, splitRatio, currUsers, sum, result['_id'], amount)
                 
-        
-                res.status(200)
-                res.setHeader("Access-Control-Allow-Origin", "http://localhost")
-                res.send("New transaction successfully created!")
+                        res.status(200)
+                        res.setHeader("Access-Control-Allow-Origin", "http://localhost")
+                        res.send("New transaction successfully created!")
+                    })
+                }
             })
-            .catch(function(err){
-                console.log(err)
-                res.status(500)
-                res.setHeader("Access-Control-Allow-Origin", "http://localhost")
-                res.send("Internal Server Error")
-            });
         }
     })
     .catch(function(err){
